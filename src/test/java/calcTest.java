c37235040bb35f4689ba048cd924cf8db15b022e
hritik arora <hritik.arora@iiitb.org>
2021-03-17 22:49:35 +0530

    calculator.log

--- src/test/java/calcTest.java
+++ src/test/java/calcTest.java
@@ -8,12 +8,10 @@ public class calcTest {
 
     calculate cal = new calculate();
     private static final double DELTA = 1e-15;
-
     @Test
     public void factorialTruePositiveTest() {
         assertEquals("Factorial for true positive", 3628800.0, cal.fact(10.0), DELTA);
     }
-
     @Test
     public void factorialFalsePositiveTest() {
         assertNotEquals("Factorial for false positive",20.0, cal.fact(5.0), DELTA);
@@ -44,7 +42,6 @@ public class calcTest {
     public void sqrtFalsePositiveTest() {
         assertNotEquals("Square root for false positive",5.0, cal.sqrt(80.0), DELTA);
     }
-
     @Test(expected = ArithmeticException.class)
     public void sqrtExcept()
     {
@@ -56,7 +53,6 @@ public class calcTest {
         assertEquals("Natural logarithm for true positive (integer)",3.4011973816621555, cal.log(30.0), DELTA);
         assertEquals("Natural logarithm for true positive (double)",2.3513752571634776, cal.log(10.5), DELTA);
     }
-
     @Test
     public void logFalsePositiveTest() {
         assertNotEquals("Natural logarithm for false positive (integer)",5.0, cal.log(50.0), DELTA);
